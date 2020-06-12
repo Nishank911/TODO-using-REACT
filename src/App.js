e62506d86f23ee046d299e3afacc80e658a0a560
Nishank Gupta <nishankgupta1234@gmail.com>
2020-06-13 00:48:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todo: "",
+    todoList: [],
+  };
+
+  addTODO = () => {
+    //unique id
+    const todo = {
+      id: 1 + Math.random(),
+      value: this.state.todo.slice(),
+    };
+
+    //copy of current list
+    const todoList = [...this.state.todoList];
+    console.log(this.state.todo);
+    if (!(todo.value == "")) {
+      //adding todo to list
+      todoList.push(todo);
+    }
+    //update state
+    this.setState({
+      todo: "",
+      todoList,
+    });
+  };
+
+  updateInput = (key, value) => {
+    this.setState({
+      [key]: value,
+    });
+  };
+  deleteTodo = (id) => {
+    //copy of list
+    const list = [...this.state.todoList];
+
+    //filter out item being deleted
+    const updatedList = list.filter((item) => item.id != id);
+    this.setState({ todoList: updatedList });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>Add A TODO</h1>
+          <input
+            type="text"
+            placeholder="TODO"
+            value={this.state.todo}
+            onChange={(e) => this.updateInput("todo", e.target.value)}
+          ></input>
+          <button onClick={() => this.addTODO()}>ADD</button>
+          <ul>
+            {this.state.todoList.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+
+                  <button onClick={() => this.deleteTodo(item.id)}>
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
